3096bd8a6e231f02cdae4d6f097e892903166547
Eric Nisoli <eric@niso.io>
2017-12-28 21:13:23 +0100

    Add hgraph basic functions
    - Create
    - Vertex and edges count

--- src/graph/hgraph.c
+++ src/graph/hgraph.c
@@ -1,1 +1,36 @@
 #include "hgraph.h"
+
+static inline void
+assert_graph_init(hgraph* g)
+{
+    assert(g != NULL && "Graph is not initialized");
+}
+
+hgraph*
+hgraph_create()
+{
+    hgraph* g = malloc(sizeof(hgraph));
+    g->v = 0;
+    g->e = 0;
+    g->vertices = malloc(sizeof(hgraph_vertex_map));
+    map_init(g->vertices);
+
+    return g;
+}
+
+int
+hgraph_vertex_count(hgraph* g)
+{
+    assert_graph_init(g);
+
+    return g->v;
+}
+
+int
+hgraph_edge_count(hgraph* g)
+{
+    assert_graph_init(g);
+
+    return g->e;
+}
+
