4084dda8bf33c31705484a6f0271207c220571b8
Eric Nisoli <eric@niso.io>
2017-12-29 16:58:46 +0100

    Add toupper string funtion

--- src/main/hague.c
+++ src/main/hague.c
@@ -1,8 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <zlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
 #include "../../libs/klib/kseq.h"
 #include "../io/reader.h"
 #include "../cmdline/cmdline.h"
 
 typedef struct gengetopt_args_info ggo_args;
+
+void
+create_de_bruijn_graph(kseq_t*, int);
 
 int
 main(int argc, char** argv)
@@ -11,5 +20,26 @@ main(int argc, char** argv)
     assert(cmdline_parser(argc, argv, &ai) == 0);
 
     return 0;
+}
 
+char*
+strupr(char* s_l)
+{
+    char* s_u = malloc(strlen(s_l) * sizeof(char) + 1);
+    int i = 0;
+    while(s_l[i] != '\0')
+    {   
+        char c;
+
+        if (s_l[i] >= 'a' && s_l[i] <= 'z')
+            c = toupper(s_l[i]);
+        else
+            c = s_l[i];
+
+        s_u[i] = c;
+        i++;
+    }
+    
+    s_u[strlen(s_l) + 1] = '\0';
+    return s_u;
 }
